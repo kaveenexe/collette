81e83508d9cba42bfef79ab96b864e735f773ce0
Gehan Sathushka <it21102196@my.sliit.lk>
2024-10-07 11:56:45 +0530

    Modified Product Listing UI

--- app/src/main/java/com/example/colllette/network/Product.kt
+++ app/src/main/java/com/example/colllette/network/Product.kt
@@ -5,6 +5,7 @@ data class Product(
     val uniqueProductId: String,
     val name: String,
     val description: String?,
+    var category: String,
     val price: Double,
     val stockQuantity: Int,
     val vendorId: String,
